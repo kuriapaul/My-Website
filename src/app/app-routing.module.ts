eec555bfa8c00edd9867dfef7ced1e7851d9aa80
kuriapaul <pauwains@gmail.com>
2019-01-30 22:10:30 +0300

    removed duplicated component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  { 
-    path: '', redirectTo: '/home',
-    pathMatch: 'full' 
-  },
-  { 
-    path: 'home',
-    component: AppComponent 
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
