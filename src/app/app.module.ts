b32027b5887e39601e01fe9209adbcc3d9ae6500
kuriapaul <pauwains@gmail.com>
2019-01-30 22:03:39 +0300

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { EducationComponent } from './education/education.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    AboutComponent,
+    ExperienceComponent,
+    PortfolioComponent,
+    EducationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
